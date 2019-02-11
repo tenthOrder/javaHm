0fd030bd6717b50c1589b4223b40a7a37aaf6e00
Anonymous <>
2019-02-11 17:38:56 +0300

    add HomeWork23

---
+++ HomeWorks/HomeWork23/src/ru/itis/Main.java
@@ -0,0 +1,28 @@
+package ru.itis;
+
+import java.util.Random;
+
+public class Main {
+
+    public static String generateRandomString() {
+        final int MAX_STRING_LENGTH = 100;
+        Random random = new Random();
+        int length = random.nextInt(MAX_STRING_LENGTH) + 1;
+        StringBuilder stringBuilder = new StringBuilder(length);
+        int[] chars = random.ints(length, '!', Character.MAX_VALUE).toArray();
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append((char) chars[i]);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] randomStrings = new String[100];
+        Map<String, Integer> map = new Map<>();
+        for (int i = 0; i < randomStrings.length; i++) {
+            randomStrings[i] = generateRandomString();
+            map.put(randomStrings[i], 0);
+        }
+        System.out.println(map.getCollisionCount());
+    }
+}
---
+++ HomeWorks/HomeWork23/src/ru/itis/Map.java
@@ -0,0 +1,39 @@
+package ru.itis;
+
+public class Map<K, V> {
+
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Entry[] entries;
+    private int collisionCount;
+
+    public Map() {
+        entries = new Entry[100];
+        collisionCount = 0;
+    }
+
+    private int getIndex(K key) {
+        int index = key.hashCode() % entries.length;
+        return index > 0 ? index : -index;
+    }
+
+    public void put(K key, V value) {
+        int index = getIndex(key);
+        if (entries[index] != null) {
+            collisionCount++;
+        }
+        entries[index] = new Entry<>(key, value);
+    }
+
+    public int getCollisionCount() {
+        return collisionCount;
+    }
+}
